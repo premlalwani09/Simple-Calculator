175e6094d18ca3810604f6c6ab3863d3a5568036
Prem Lalwani <premprakashlalwani@gmail.com>
2024-05-24 00:37:49 +0530

    Calculator Added

---
+++ script.js
@@ -0,0 +1,26 @@
+function calculate() {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let operator = document.getElementById("operator").value;
+
+    let result;
+
+    
+    if (operator === "+") {
+        result = num1 + num2;
+    } else if (operator === "-") {
+        result = num1 - num2;
+    } else if (operator === "*") {
+        result = num1 * num2;
+    } else if (operator === "/") {
+        if (num2 !== 0) {
+            result = num1 / num2;
+        } else {
+            result = "Error: Division by zero";
+        }
+    } else {
+        result = "Invalid operator";
+    }
+
+    document.getElementById("result").innerText = "Result: " + result;
+}
